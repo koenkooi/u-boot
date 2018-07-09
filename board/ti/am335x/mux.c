05b203c2e81c8cb7f9e154f16f549265f6f7eb65
Koen Kooi <koen@dominion.thruhere.net>
2018-07-09 08:31:34 +0200

    add beaglebone enhanced support
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- board/ti/am335x/mux.c
+++ board/ti/am335x/mux.c
@@ -381,6 +381,13 @@ void enable_board_pin_mux(void)
 		configure_module_pin_mux(mmc0_pin_mux_sk_evm);
 	} else if (board_is_bone_lt()) {
 		/* Beaglebone LT pinmux */
+		if(board_is_bben()) {
+			/* SanCloud Beaglebone LT Enhanced pinmux */
+			configure_module_pin_mux(rgmii1_pin_mux);
+		} else {
+			/* Beaglebone LT pinmux */
+			configure_module_pin_mux(mii1_pin_mux);
+		}
 		configure_module_pin_mux(mii1_pin_mux);
 		configure_module_pin_mux(mmc0_pin_mux);
 #if defined(CONFIG_NAND) && defined(CONFIG_EMMC_BOOT)
